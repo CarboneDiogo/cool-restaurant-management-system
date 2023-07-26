42c7b4c7da65f5b02e2fe601c74318b1280b4c52
Diogo Carbone <diogo.carbone@gmail.com>
2023-07-26 13:19:56 -0600

    project fix

--- cool-restaurant-management-system/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace cool_restaurant_management_system;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
