3c8fc6e6f81e081152bb4fb4fff0d49bde59ed35
Diogo Carbone <diogo.carbone@gmail.com>
2023-07-26 13:22:31 -0600

    Changed project template
    Recreated the project with a Blazor template

---
+++ cool-restaurant-management-system/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace cool_restaurant_management_system;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ cool-restaurant-management-system/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace cool_restaurant_management_system;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
