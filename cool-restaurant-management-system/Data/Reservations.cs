3cffffcaf5eaf9a9b4c2c06d6a0123a19d84fe02
Diogo Carbone <diogo.carbone@gmail.com>
2023-08-07 01:43:56 -0600

    rebuilt all the pages for better UI
    Since we're breaking down the first page I decided to rebuild the UI - the content is better distributed now. Commiting to main since this is gonna be the basis for the rest of the app.

--- cool-restaurant-management-system/Data/Reservations.cs
+++ cool-restaurant-management-system/Data/Reservations.cs
@@ -8,32 +8,28 @@ namespace cool_restaurant_management_system.Data
 {
 	public class Reservations
 	{
-        private int reservation_Id;
-        private int table_Id;
-        private int guest_Id;
-        private int number_of_Guests;
-        private string date_Time;
 
-		public int Reservation_Id { get => reservation_Id; set => reservation_Id = value; }
-		public int Table_Id { get => table_Id; set => table_Id = value; }
-		public int Guest_Id { get => guest_Id; set => guest_Id = value; }
-		public int Number_of_Guests { get => number_of_Guests; set => number_of_Guests = value; }
-		public string Date_Time { get => date_Time; set => date_Time = value; }
+        public int Reservation_Id { get; set; }
+		public int Table_Id { get; set; }
+		public int Guest_Id { get; set; }
+		public int Number_of_Guests { get; set; }
+		public DateTime Reserved_From { get; set; }
+		public DateTime Reserved_To { get; set; }
 
         public Reservations()
         {
             
         }
 
-		public Reservations(int reservation_Id, int table_Id, int guest_Id, int number_of_Guests, string date_Time)
-		{
-			this.reservation_Id = reservation_Id;
-			this.table_Id = table_Id;
-			this.guest_Id = guest_Id;
-			this.number_of_Guests = number_of_Guests;
-			this.date_Time = date_Time;
-		}
-        
+        public Reservations(int reservation_Id, int table_Id, int guest_Id, int number_of_Guests, DateTime reserved_From, DateTime reserved_To)
+        {
+            Reservation_Id = reservation_Id;
+            Table_Id = table_Id;
+            Guest_Id = guest_Id;
+            Number_of_Guests = number_of_Guests;
+            Reserved_From = reserved_From;
+            Reserved_To = reserved_To;
+        }
     }
 }
 
